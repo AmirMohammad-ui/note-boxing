667a6d0a5137d5d57b1c8e3fc9241c2838bb01fe
AmirMohammad-ui <mirzaii4046@gmail.com>
2021-05-31 23:12:46 +0430

    feat: custom Validator class

--- client/src/util/validating-newPlan.ts
+++ client/src/util/validating-newPlan.ts
@@ -9,6 +9,18 @@ interface ErrMsg {
   field: string,
   message: string
 }
+enum ErrTypes {
+  required = 'REQUIRED',
+  isString = 'STRING',
+  isNumber = 'NUMBER',
+  isBool = 'BOOLEAN',
+  min = 'MINIMUM',
+  max = 'MAXIMUM',
+  email = 'EMAIL',
+  date = 'DATE',
+  alphanumeric = "ALPHANUM",
+  file = "File"
+}
 
 class Validate {
   private errorMessages : ErrMsg[] = []
@@ -24,9 +36,9 @@ class Validate {
     const {value, errorMessages, field} = validateObj
     if (!value) {
       errorMessages.push({
-        type: 'required', 
+        type: ErrTypes.required, 
         field, 
-        message: `${field} is required, please enter the appropriate value `
+        message: `${field} is required, please enter the appropriate value.`
       })
     }
     return this
@@ -37,7 +49,7 @@ class Validate {
       errorMessages.push({type: 'min-number', field, message: `Minimum valid value for ${field} is ${min}.`})
     } else if(typeof value === "string" && value.length<min) {
       errorMessages.push({
-        type: 'min-string', 
+        type: ErrTypes.min, 
         field, 
         message: `Minimum number of charachtors for ${field} is ${min}.`
       })
@@ -48,18 +60,106 @@ class Validate {
     const {value, errorMessages, field} = new Validate()
     if(value instanceof File && value.size > max) {
       errorMessages.push({
-        type: 'max-size', 
+        type: ErrTypes.max, 
         field, 
         message: `Maximum size for ${field} is ${max}.`
       })
-    }
+    } else if(typeof value === "string" && value.length>max) {
+      errorMessages.push({
+        type: ErrTypes.max, 
+        field, 
+        message: `Maximum number of charachtors for ${field} is ${max}.`
+      })
+    } 
   }
   static email() {}
   static alphanumeric() {}
-  static string() {}
-  static number() {}
-  static boolean() {}
-  static file() {}
+  static string() {
+    const {errorMessages,value,field} = new Validate()
+    if(typeof value !== 'string') {
+      errorMessages.push({
+        type: "isString",
+        field,
+        message: `This field must be a string.`
+      })
+    }
+  }
+  static number() {
+    const {errorMessages,value,field} = new Validate()
+    if(typeof value !== "number") {
+      errorMessages.push({
+        type: "isNumber",
+        field,
+        message: `This field must be a number.`
+      })
+    }
+  }
+  static date(options: {
+    "yyyy/mm/dd"?: boolean;
+    "yyyy-mm-dd"?: boolean;
+    "Mon dd yyyy"?: boolean;
+    "Month dd yyyy"?: boolean
+  }={"yyyy-mm-dd":true}) {
+    const { value,field,errorMessages } = new Validate()
+    if (options['Mon dd yyyy']) {
+      const datePattern = /(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s[\d\d]\s[\d\d\d\d]/
+      if(!value.toString().match(datePattern)) {
+        errorMessages.push({
+        type: ErrTypes.date,
+        field,
+        message: `${value} is not a valid Date.`
+        })
+      }
+    } else if (options['Month dd yyyy']) {
+      const datePattern = /(January|February|March|May|June|July|November|December|April|August|September|October)\s[\d\d]\s[\d\d\d\d]/
+      if(!value.toString().match(datePattern)) {
+        errorMessages.push({
+        type: ErrTypes.date,
+        field,
+        message: `${value} is not a valid Date.`
+        })
+      }
+    } else if (options["yyyy-mm-dd"]) { 
+      const datePattern = /\d\d\d\d-\d\d-\d\d/
+      if(!value.toString().match(datePattern)) {
+        errorMessages.push({
+        type: ErrTypes.date,
+        field,
+        message: `${value} is not a valid Date.`
+        })
+      }
+    } else if (options['yyyy/mm/dd']) {
+      const datePattern = /\d\d\d\d\/\d\d\/\d\d]/
+      if(!value.toString().match(datePattern)) {
+        errorMessages.push({
+        type: ErrTypes.date,
+        field,
+        message: `${value} is not a valid Date.`
+        })
+      }
+    }
+  }
+  static boolean() {
+    const bool = ['true','false',true,false,1,0]
+    const { value,field,errorMessages } = new Validate()
+    if(!(value as any in bool)) {
+      errorMessages.push({
+        type: '',
+        field,
+        message: ``
+      })
+    }
+  }
+  static file() {
+    const { value,field,errorMessages } = new Validate()
+    if(!(value instanceof File)) {
+      errorMessages.push({
+        type: ErrTypes.file,
+        field,
+        message: `${value} is not a file.`
+      })
+    }
+  }
 
   static errors(options: {messagesOnly?: boolean;fieldsOnly?: boolean; }) {
     const {errorMessages} = new Validate()
