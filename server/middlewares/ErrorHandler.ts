40256ce3c75947c82b039ae2ad4cd0ba51c53ee6
amirmuha <mirzaii4046@gmail.com>
2021-12-12 20:19:23 +0330

    fix: logging the errors only in development

--- server/middlewares/ErrorHandler.ts
+++ server/middlewares/ErrorHandler.ts
@@ -1,19 +1,22 @@
 // import ErrorHandler from "../utilities/ErrorHandler"
-import * as chalk from "chalk"
+import * as chalk from "chalk";
 
-export default function(err,_,res,next) {
-  console.log(chalk.yellow(err.statusCode))
-  console.log(chalk.yellow(err.status))
-  console.log(chalk.redBright(err.message))
-  console.log(chalk.cyan(err.name))
-  console.log(chalk.grey(err.stack))
-  
+export default function (err, _, res, next) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(chalk.yellow(err.statusCode));
+    console.log(chalk.yellow(err.status));
+    console.log(chalk.redBright(err.message));
+    console.log(chalk.cyan(err.name));
+    console.log(chalk.grey(err.stack));
+  }
+
   res.status(err.statusCode || res.statusCode || 500).json({
     success: 0,
     status: err.status,
     statusCode: err.statusCode,
     message: err.message,
     name: err.name,
-    stack: err.stack
-  })
+    stack: err.stack,
+  });
 }
+
--- server/middlewares/asyncErrorHandler.ts
+++ server/middlewares/asyncErrorHandler.ts
@@ -1,10 +1,13 @@
-export default handler => {
-  return async(req,res,next) => {
+export default (handler) => {
+  return async (req, res, next) => {
     try {
-      await handler(req,res,next)
-    } catch(err) {
-      console.log(err.message)
-      next(err)
+      await handler(req, res, next);
+    } catch (err) {
+      if (process.env.NODE_ENV === "development") {
+        console.log(err.message);
+      }
+      next(err);
     }
-  }
-}
+  };
+};
+
