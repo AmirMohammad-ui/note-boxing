0ba4a27a3466b10dc6a5b3845033091c2457420e
amirmuha <mirzaii4046@gmail.com>
2021-12-12 20:14:01 +0330

    feat: configured baseUrl in a single file

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,31 +1,32 @@
-import { createApp } from "vue"
-import router from "./router/index"
-import axios from "axios"
-import App from "./App.vue"
-import store from "./store/index"
-import BaseInput from "./components/UI/BaseInput.vue"
-import BaseTextarea from "./components/UI/BaseTextarea.vue"
-import BaseButton from "./components/UI/BaseButton.vue"
-import Container from "./components/UI/Container.vue"
-import Seperator from "./components/UI/Seperator.vue"
-import BaseModal from "./components/UI/BaseModal.vue"
-import BaseAlert from "./components/UI/BaseAlert.vue"
-import "./tailwind.css"
+import { createApp } from "vue";
+import router from "./router/index";
+import App from "./App.vue";
+import store from "./store/index";
+import BaseInput from "./components/UI/BaseInput.vue";
+import axios from "./util/axios";
+import BaseTextarea from "./components/UI/BaseTextarea.vue";
+import BaseButton from "./components/UI/BaseButton.vue";
+import Container from "./components/UI/Container.vue";
+import Seperator from "./components/UI/Seperator.vue";
+import BaseModal from "./components/UI/BaseModal.vue";
+import BaseAlert from "./components/UI/BaseAlert.vue";
+import "./tailwind.css";
 
-const app = createApp(App)
+const app = createApp(App);
 declare module "@vue/runtime-core" {
-  export interface ComponentCustomProperties { $axios: typeof axios; }
+  export interface ComponentCustomProperties {
+    $axios: typeof axios;
+  }
 }
 
-axios.defaults.baseURL = "http://localhost:3400/api"
-app.config.globalProperties.$axios = axios
-app.component("base-input", BaseInput)
-app.component("base-button", BaseButton)
-app.component("base-textarea", BaseTextarea)
-app.component("container", Container)
-app.component("seperator", Seperator)
-app.component("base-modal", BaseModal)
-app.component("base-alert", BaseAlert)
-app.use(router)
-app.use(store)
-app.mount("#app")
+app.config.globalProperties.$axios = axios;
+app.component("base-input", BaseInput);
+app.component("base-button", BaseButton);
+app.component("base-textarea", BaseTextarea);
+app.component("container", Container);
+app.component("seperator", Seperator);
+app.component("base-modal", BaseModal);
+app.component("base-alert", BaseAlert);
+app.use(router);
+app.use(store);
+app.mount("#app");
--- client/src/store/Plans/index.ts
+++ client/src/store/Plans/index.ts
@@ -1,18 +1,17 @@
-import axios from "axios"
-axios.defaults.baseURL = "http://localhost:3400/api"
+import axios from "../../util/axios";
 
-import currentPlans from "./CurrentPlans/index"
-import dailyPlans from "./Tables/Daily/index"
-import monthlyPlans from "./Tables/Monthly/index"
-import yearlyPlans from "./Tables/Yearly/index"
+import currentPlans from "./CurrentPlans/index";
+import dailyPlans from "./Tables/Daily/index";
+import monthlyPlans from "./Tables/Monthly/index";
+import yearlyPlans from "./Tables/Yearly/index";
 
 enum PlanTypes {
-  TODAY="todayPlans" ,
-  CURMONTH="currentMonthPlans" ,
-  CURYEAR="currentYearPlans" ,
-  DAILY="dailyPlans" ,
-  MONTHLY="monthlyPlans" ,
-  YEARLY="yearlyPlans" ,
+  TODAY = "todayPlans",
+  CURMONTH = "currentMonthPlans",
+  CURYEAR = "currentYearPlans",
+  DAILY = "dailyPlans",
+  MONTHLY = "monthlyPlans",
+  YEARLY = "yearlyPlans",
 }
 
 interface State {
@@ -22,8 +21,8 @@ interface State {
     [PlanTypes.CURYEAR]: any[];
     [PlanTypes.DAILY]: any[];
     [PlanTypes.MONTHLY]: any[];
-    [PlanTypes.YEARLY]: any[]
-  }
+    [PlanTypes.YEARLY]: any[];
+  };
 }
 export default {
   namespaced: true,
@@ -31,7 +30,7 @@ export default {
     currentPlans,
     dailyPlans,
     monthlyPlans,
-    yearlyPlans
+    yearlyPlans,
   },
   state: {
     plans: {
@@ -41,70 +40,75 @@ export default {
       [PlanTypes.DAILY]: [],
       [PlanTypes.MONTHLY]: [],
       [PlanTypes.YEARLY]: [],
-    } 
+    },
   },
   getters: {
-    getTodayPlans: ({plans}:any) => {
-      return plans[PlanTypes.TODAY]
+    getTodayPlans: ({ plans }: any) => {
+      return plans[PlanTypes.TODAY];
     },
-    getCurrentMonthPlans: ({plans}:any) => {
-      return plans[PlanTypes.CURMONTH]
+    getCurrentMonthPlans: ({ plans }: any) => {
+      return plans[PlanTypes.CURMONTH];
     },
-    getCurrentYearPlans: ({plans}:any) => {
-      return plans[PlanTypes.CURYEAR]
+    getCurrentYearPlans: ({ plans }: any) => {
+      return plans[PlanTypes.CURYEAR];
     },
-    getDailyPlans({plans}:any) {
-      return plans[PlanTypes.DAILY] 
+    getDailyPlans({ plans }: any) {
+      return plans[PlanTypes.DAILY];
     },
-    getMonthlyPlans({plans}:any) {
-      return plans[PlanTypes.MONTHLY] 
-    }, 
-    getYearlyPlans({plans}:any) {
-      return plans[PlanTypes.YEARLY]
-    }
+    getMonthlyPlans({ plans }: any) {
+      return plans[PlanTypes.MONTHLY];
+    },
+    getYearlyPlans({ plans }: any) {
+      return plans[PlanTypes.YEARLY];
+    },
   },
   mutations: {
     setToday(state: State, payload: any) {
-      state.plans[PlanTypes.TODAY] = payload
-    }, 
+      state.plans[PlanTypes.TODAY] = payload;
+    },
     setCurrentMonth(state: State, payload: any) {
-      state.plans[PlanTypes.CURMONTH] = payload
+      state.plans[PlanTypes.CURMONTH] = payload;
     },
     setCurrentYear(state: State, payload: any) {
-      state.plans[PlanTypes.CURYEAR] = payload
-    }, 
+      state.plans[PlanTypes.CURYEAR] = payload;
+    },
     setDaily(state: State, payload: any) {
-      state.plans[PlanTypes.DAILY] = payload
-    }, 
+      state.plans[PlanTypes.DAILY] = payload;
+    },
     setMonthly(state: State, payload: any) {
-      state.plans[PlanTypes.MONTHLY] = payload
-    }, 
+      state.plans[PlanTypes.MONTHLY] = payload;
+    },
     setYearly(state: State, payload: any) {
-      state.plans[PlanTypes.YEARLY] = payload
-    }
+      state.plans[PlanTypes.YEARLY] = payload;
+    },
   },
   actions: {
-    deletePlan({state,dispatch}:any,payload:string){
-      axios.delete("/delete",{
-        params: {id: payload}
-      }).then(({data: {type}}) => {
-        if(type === "daily"){
-          dispatch("plans/currentPlans/fetchTodaysPlans",null,{ root: true })
-          dispatch("plans/dailyPlans/fetchPlans",null,{ root: true })
-        } else if(type === "monthly") {
-          dispatch("plans/currentPlans/fetchCurrentMonthPlans",null,{ root: true })
-          dispatch("plans/monthlyPlans/fetchPlans",null,{ root: true })
-        } else if(type === "yearly") {
-          dispatch("plans/currentPlans/fetchCurrentYearPlans",null,{ root: true })
-          dispatch("plans/yearlyPlans/fetchPlans",null,{ root: true })
-        }
-      }).catch(console.error)
+    deletePlan({ state, dispatch }: any, payload: string) {
+      axios
+        .delete("/delete", {
+          params: { id: payload },
+        })
+        .then(({ data: { type } }) => {
+          if (type === "daily") {
+            dispatch("plans/currentPlans/fetchTodaysPlans", null, {
+              root: true,
+            });
+            dispatch("plans/dailyPlans/fetchPlans", null, { root: true });
+          } else if (type === "monthly") {
+            dispatch("plans/currentPlans/fetchCurrentMonthPlans", null, {
+              root: true,
+            });
+            dispatch("plans/monthlyPlans/fetchPlans", null, { root: true });
+          } else if (type === "yearly") {
+            dispatch("plans/currentPlans/fetchCurrentYearPlans", null, {
+              root: true,
+            });
+            dispatch("plans/yearlyPlans/fetchPlans", null, { root: true });
+          }
+        })
+        .catch(console.error);
     },
-    editPlan(){
-      
-    },
-    completePlan() {
-
-    }
-  }
-}
+    editPlan() {},
+    completePlan() {},
+  },
+};
