83dbcbb7c520ca887645ce8d017bb882114d2464
amirmuha <mirzaii4046@gmail.com>
2021-12-13 19:50:56 +0330

    feat: initialized passport auth with github and google

---
+++ server/apis/auth.ts
@@ -0,0 +1,44 @@
+import * as passport from "passport";
+import * as express from "express";
+import asyncHandler from "../middlewares/asyncErrorHandler";
+
+const router = express.Router();
+
+// GOOGLE
+router.route("/auth/google").get(
+  asyncHandler(
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
+    })
+  )
+);
+router.route("/auth/google/callback").get(
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    successRedirect: "/",
+  }),
+  asyncHandler((req, res, next) => {
+    console.log(req.query);
+    res.send("User authenticated.");
+  })
+);
+
+// GITHUB
+router.route("/auth/github").get(
+  asyncHandler(
+    passport.authenticate("github", {
+      scope: ["user:email"],
+    })
+  )
+);
+router.route("/auth/github/callback").get(
+  passport.authenticate("github", {
+    failureRedirect: "/login",
+    successRedirect: "/",
+  }),
+  asyncHandler((req, res, next) => {
+    console.log(req.query);
+    res.send("User authenticated.");
+  })
+);
+export default router;
