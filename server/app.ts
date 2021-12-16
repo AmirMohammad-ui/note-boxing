463c67c680d7e703880f244cbd1b86899fa1cad5
amirmuha <mirzaii4046@gmail.com>
2021-12-16 20:17:45 +0330

    feat: passport authentication initialized

--- server/app.ts
+++ server/app.ts
@@ -8,7 +8,9 @@ import * as cors from "cors";
 import * as fileUpload from "express-fileupload";
 import * as RedisConnect from "connect-redis";
 import * as session from "express-session";
+import * as passport from "passport";
 import redis from "./utilities/redis-client";
+import setupPassport from "./utilities/passport";
 import * as helmet from "helmet";
 import * as sanitize from "express-mongo-sanitize";
 import * as path from "path";
@@ -18,38 +20,58 @@ const app = express();
 
 import users from "./apis/users";
 import plan from "./apis/plan";
+import auth from "./apis/auth";
 
-const RedisStore = RedisConnect(session);
+// --------------- GENERAL MIDDLEWARES
 app.use(express.json());
 app.use(
   "/static/images",
   express.static(path.join(__dirname, "./uploads/images/"))
 );
-app.use(cors());
-app.use(helmet());
-app.use(sanitize());
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", true);
+}
+// --------------- SETTING UP SESSION
+const RedisStore = RedisConnect(session);
 app.use(
   session({
     name: "session_id_",
     secret: process.env.SESSION_SECRET!,
     store: new RedisStore({ client: redis }),
-    saveUninitialized: true,
-    resave: true,
+    saveUninitialized: false,
+    resave: false,
     cookie: {
       secure: process.env.NODE_ENV === "production",
-      sameSite: true,
+      sameSite: "none",
       httpOnly: true,
       maxAge: +process.env.SESSION_MAX_AGE! * 24 * 1000 * 60 * 60,
     },
   })
 );
+// --------------- SETTING UP PASSPORT
+setupPassport(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+// --------------- SECURITY
+app.use(
+  cors({
+    origin: [
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:8080"
+        : "https://demo.portfolio.amirmuha.com/note-boxing",
+    ],
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(sanitize());
 app.use(
   fileUpload({
     limits: {
       fileSize: 1 * 1024 * 1024,
     },
-    useTempFiles: true,
-    tempFileDir: "./temp/",
+    useTempFiles: false,
+    // tempFileDir: "./temp/",
   })
 );
 app.use(morgan("dev"));
@@ -57,6 +79,7 @@ app.use(morgan("dev"));
 // app.use("/api",users,plan)
 app.use("/api", users);
 app.use("/api", plan);
+app.use("/api", auth);
 
 app.all("*", (req, _, next) => {
   next(new ErrorHandler(`NOT FOUND: ${req.originalUrl}`, 404));
---
+++ server/utilities/passport.ts
@@ -0,0 +1,48 @@
+import { PassportStatic } from "passport";
+import {
+  Strategy as GithubStrategy,
+  Profile as GithubProfile,
+} from "passport-github2";
+import User from "../models/users";
+import { OAuth2Strategy } from "passport-google-oauth";
+
+export default (passport: PassportStatic) => {
+  // --------- GITHUB AUTH
+  passport.use(
+    new GithubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        callbackURL:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3400/api/auth/github/callback"
+            : "https://demo.portfolio.amirmuha.com/note-boxing/api/auth/github/callback",
+      },
+      (_, _2, profile: GithubProfile, done) => {
+        console.log(profile);
+      }
+    )
+  );
+  // --------- GOOGLE AUTH
+  const GoogleStrategy = OAuth2Strategy;
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        callbackURL:
+          process.env.NODE_ENV === "development"
+            ? "http://localhost:3400/api/auth/google/callback"
+            : "https://demo.portfolio.amirmuha.com/note-boxing/api/auth/google/callback",
+      },
+      function (token, tokenSecret, profile, done) {
+        console.log("token", token);
+        console.log("token secret", tokenSecret);
+        console.log("profile", profile);
+        // User.findById({ googleId: profile.id }, function (err, user) {
+        // return done(err, user);
+        // });
+      }
+    )
+  );
+};
