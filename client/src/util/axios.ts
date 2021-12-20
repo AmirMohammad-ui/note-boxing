35cfc450baf11b3308192051ae29a24cf0c01786
amirmuha <mirzaii4046@gmail.com>
2021-12-20 20:33:57 +0330

    fix: fixed api url

--- client/src/util/axios.ts
+++ client/src/util/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL =  "https://note-boxing.amirmuha.com/api";
+export const baseURL =  "https://note-boxing.portfolio.amirmuha.com/api";
 axios.defaults.baseURL = baseURL;
 
 export default axios;
